7e9659c5240fd612011de4079b35d403d8f0dc92
shinestar58 <55105580@qq.com>
2026-07-17 16:02:46 +0800

    add some files

---
+++ Scripts/Potions/BigDogPotionPool.cs
@@ -0,0 +1,13 @@
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace BigDog.Scripts;
+
+public class BigDogPotionPool : TypeListPotionPoolModel
+{
+    // 描述中使用的能量图标。大小为24x24。
+    public override string? TextEnergyIconPath => "res://BigDog/images/energy/energy_big_dog.png";
+    // tooltip和卡牌左上角的能量图标。大小为74x74。
+    public override string? BigEnergyIconPath => "res://BigDog/images/energy/energy_big_dog_big.png";
+
+    public override string EnergyColorName => "BigDog";
+}
